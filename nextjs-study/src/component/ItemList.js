e1c974077abc70b4026fcc6ab58b5821effed685
HYO00 <hyo02222@gmail.com>
2022-02-16 16:37:29 +0900

    Dynamic Routes

--- nextjs-study/src/component/ItemList.js
+++ nextjs-study/src/component/ItemList.js
@@ -8,23 +8,22 @@ export default function ItemList({ list }) {
     <div>
       <Grid columns={3}>
         <Grid.Row>
-          {list &&
-            list.map((item) => (
-              <Grid.Column key={item.name}>
-                <Link href={`/view/${item.id}`}>
-                  <a>
-                    <div className={styles.wrap}>
-                      <img
-                        src={item.sprites.front_default}
-                        alt={item.name}
-                        className={styles.item_img}
-                      />
-                      <strong className={styles.item_name}>{item.name}</strong>
-                    </div>
-                  </a>
-                </Link>
-              </Grid.Column>
-            ))}
+          {list.map((item) => (
+            <Grid.Column key={item.name}>
+              <Link href={`/view/${item.id}`}>
+                <a>
+                  <div className={styles.wrap}>
+                    <img
+                      src={item.sprites.front_default}
+                      alt={item.name}
+                      className={styles.item_img}
+                    />
+                    <strong className={styles.item_name}>{item.name}</strong>
+                  </div>
+                </a>
+              </Link>
+            </Grid.Column>
+          ))}
         </Grid.Row>
       </Grid>
     </div>
